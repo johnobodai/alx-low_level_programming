87c12d9cf5f8bd6a742672b07dc5e7615bf83b0f
johnobodai <j.obodai@alustudent.com>
2023-07-07 17:11:17 +0000

    Add description for times_table function in .c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * time_table - Prints the multiplication table of 0 to 9
+ * times_table - Prints the multiplication table of 0 to 9
  *
- * Descriptions: Prints the multiplication tabel of 0 to 9
- *               it does so by looping through 0 to 9 to for a table
- *               it then prints using _putchar as the output
- * Return: Void
+ * Description: This function prints the multiplication table of numbers from
+ *              0 to 9 in a grid format. Each number is separated by a comma
+ *              and a space.The table is printed using the _putchar function.
  */
+
 void times_table(void)
 {
 	int i;
@@ -15,11 +15,13 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
+
 			if (k < 10)
 			{
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else
@@ -28,7 +30,11 @@ void times_table(void)
 				_putchar(k % 10 + '0');
 			}
 
-			_putchar(' ');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
