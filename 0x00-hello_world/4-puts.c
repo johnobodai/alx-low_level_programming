cacc2c68f1df1188b76b8ffb8e2bcab747f7c8ee
johnobodai <j.obodai@alustudent.com>
2023-07-13 18:13:22 +0000

    Resolved betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 char message[] = "\"Programming is like building a multilingual puzzle";
+puts(message);
 
-int main() {
-  puts(message);
-
-  return 0;
+return (0);
 }
