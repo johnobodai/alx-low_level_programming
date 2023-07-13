ac674a604f3e58de14ca549ad44e09c1a0074d03
johnobodai <j.obodai@alustudent.com>
2023-07-13 17:59:40 +0000

    Modified code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-char message[] = "\"Programming is like building a multilingual puzzle\"";
+char message[] = "\"Programming is like building a multilingual puzzle";
 
 int main() {
   puts(message);
