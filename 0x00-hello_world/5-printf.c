b8c3f31a0553e15da1ba7c6f05b551e640439d73
johnobodai <j.obodai@alustudent.com>
2023-07-14 05:12:47 +0000

    Hello, printf task completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-char message[] = "with proper grammer, but the outcome is a piece of art\n"
-printf(message);
+char message[] = "with proper grammer, but the outcome is a piece of art\n";
+printf("%s", message);
 
 return (0);
 }
