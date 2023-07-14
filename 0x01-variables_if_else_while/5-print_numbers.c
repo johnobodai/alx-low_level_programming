ef4bbc84c3d4075ec9c761aaeb8ebe542e8e24e5
johnobodai <j.obodai@alustudent.com>
2023-07-14 15:05:43 +0000

    Prints all single digits num of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -
+ *
+ * what function does
+ *
+ *Returns: *
+ */
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
