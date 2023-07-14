2a079c8367179328a6a40ea602b4a3c323a0e98c
johnobodai <j.obodai@alustudent.com>
2023-07-14 13:44:06 +0000

    Minor spelling errors check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,13 @@ n = rand() - RAND_MAX / 2;
 r = n % 10;
 if (r > 5)
 {
-printf("Last digit if %i is %i and is greater than 5\n", n,r);
+printf("Last digit of %i is %i and is greater than 5\n", n,r);
 }
 else if (r < 6 && r != 0)
 {
-printf("Last digit of %i is %i and id less then 6 and not 0\n", n,r);
+printf("Last digit of %i is %i and is less then 6 and not 0\n", n,r);
 }
 else
-printf("Last digit of %i is 0 and is 0", n);
+printf("Last digit of %i is 0 and is 0\n", n);
 return (0);
 }
