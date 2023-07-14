771a91a89ff8eb038f91f8fcaf7af06ef67b6930
johnobodai <j.obodai@alustudent.com>
2023-07-14 06:02:26 +0000

    include <stdio.h>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * This function prints to the stout
- *
- * Return : Always 1
+ * Return: Always 1.
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
 }
 
