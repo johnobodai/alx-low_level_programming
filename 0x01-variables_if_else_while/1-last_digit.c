ec15170bec80d5e7bafa63ede12ae4ffe5a85db3
johnobodai <j.obodai@alustudent.com>
2023-07-14 13:45:16 +0000

    Spelling error check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n,r);
 }
 else if (r < 6 && r != 0)
 {
-printf("Last digit of %i is %i and is less then 6 and not 0\n", n,r);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n,r);
 }
 else
 printf("Last digit of %i is 0 and is 0\n", n);
