b554b104603c9ccdc8ebc3acf0ae100db2b48116
johnobodai <j.obodai@alustudent.com>
2023-07-14 13:46:17 +0000

    Betty compliance check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main -
  *
- * what 
+ * what
  *
  * Returns : Always 0 (success)
  */
@@ -18,11 +18,11 @@ n = rand() - RAND_MAX / 2;
 r = n % 10;
 if (r > 5)
 {
-printf("Last digit of %i is %i and is greater than 5\n", n,r);
+printf("Last digit of %i is %i and is greater than 5\n", n, r);
 }
 else if (r < 6 && r != 0)
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n,r);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, r);
 }
 else
 printf("Last digit of %i is 0 and is 0\n", n);
