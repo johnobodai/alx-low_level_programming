f74fb7865144304de8747114c7ee770ee3037308
johnobodai <j.obodai@alustudent.com>
2023-07-14 05:56:19 +0000

    Added advance task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints to the stout
+ *
+ * Return : Always 1
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+return (1);
+}
+
