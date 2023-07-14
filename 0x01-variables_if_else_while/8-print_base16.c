c9d659e488f8ee7e661fc52a3ad7e8d375a61a4b
johnobodai <j.obodai@alustudent.com>
2023-07-14 17:16:18 +0000

    Betty documentation style

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
+
 /**
- * main -
+ * main - Entry point of the program
  *
- * what the function does
+ * Description: Prints all numbers of base 16 in lowercase, followed by a new line.
  *
- *Returns:
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char digit;
-for (digit = '0'; digit <= '9'; digit++)
-{
-putchar(digit);
-}
-for (digit = 'a'; digit <= 'f'; digit++)
-{
-putchar(digit);
-}
-putchar('\n');
+	char digit;
 
-return (0);
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+	{
+		putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
 
