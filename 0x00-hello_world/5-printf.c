e8278ac913c335c317839c095f745402d6cb090f
johnobodai <j.obodai@alustudent.com>
2023-07-14 05:35:01 +0000

    Added documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main - main block
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char message[] = "with proper grammer, but the outcome is a piece of art,\n";
+char message[] = "with proper grammar, but the outcome is a piece of art,\n";
 printf("%s", message);
 
 return (0);
