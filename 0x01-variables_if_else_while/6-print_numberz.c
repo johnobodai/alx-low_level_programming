a306bca64b932ed32791ea89a77ebcb578a184ed
johnobodai <j.obodai@alustudent.com>
2023-07-15 06:01:47 +0000

    Betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all single digit numbers of base 10 starting from 0 using a loop.
+ * Description: Prints all single digit numbers of 
+ * 			base 10 starting from 0 using a loop.
  *
  * Return: Always 0 (Success)
  */
