285346cb9707c1dd6f30c752c59619e70b903daa
johnobodai <j.obodai@alustudent.com>
2023-07-18 15:04:36 +0000

    Added _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description : Prints _putchar followed by a new line
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
