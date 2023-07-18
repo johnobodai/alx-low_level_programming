812c5a795e5e96df4d727977753129c52025a3d4
johnobodai <j.obodai@alustudent.com>
2023-07-18 15:56:36 +0000

    Added a header file and edited the 0...file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point of the program
  *
@@ -9,7 +9,15 @@
 
 int main(void)
 {
-	puts("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
