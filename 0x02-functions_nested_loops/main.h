fa3f4dcba4eec4bbf5e2115b5003f546f78277ea
johnobodai <j.obodai@alustudent.com>
2023-07-19 06:42:03 +0000

    Add prototype for print_last_digit function to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
 #ifndef _main_h
 #define _main_h
 
-int _putchar(char c);
+int _putchar(char i);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int i);
+int _isalpha(int i);
+int print_sign(int i);
 int _abs(int);
+int print_last_digit(int i)
 
 #endif
