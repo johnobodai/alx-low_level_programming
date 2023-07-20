2d9074f7e6a5eca3ed997b9f361b1cbe7bd1806d
johnobodai <j.obodai@alustudent.com>
2023-07-20 14:40:09 +0000

    Add and implements 0-isupper.c which check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper() - checks for uppercase charater
+ *@c: the character to be checked
+ *
+ * Returns : i
+ */
+
+int _isupper(int c)
+{
+	if (c >= A || c <= Z)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef "_main_h"
+#define "_main_h"
+
+// prints to stdout just like putchar //
+int _putchar(char i);
+
+// checks for uppercase character //
+int _isupper(int c);
+
+
+
+
+
+#endif
+
