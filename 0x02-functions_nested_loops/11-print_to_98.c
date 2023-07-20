274acf453de10d928ffb5d838b257f423bc584d5
johnobodai <j.obodai@alustudent.com>
2023-07-20 11:53:03 +0000

    Removed swp files

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,17 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (n = n; n <= 97; n++)
-		printf("%d, ", n);
+		for (n = n; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
 	}
 	else
-	for (n = n; n > 98; n--)
-		printf("%d, ", n);
-		printf("98\n");
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
+			printf("98\n");
+		}
+	}
 }
