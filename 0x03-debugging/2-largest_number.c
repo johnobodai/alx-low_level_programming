1a0f4d27be21383da251fe4b31e23f9ce8bf0490
johnobodai <j.obodai@alustudent.com>
2023-07-20 09:08:48 +0000

    Fix largest_number function to correctly fine the largest of three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,25 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (a > c && c > b)
-	{
-		largest = a:
-	}
-	else if (b > c && c > a)
-	{
-		largest = b
-	}
 	else
 	{
-		largest = c
+		largest = c:
 	}
-	return (largest);
+	return (largest)
 }
