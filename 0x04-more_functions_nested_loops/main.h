1a5f6d18312c987274789b0fab973cc0108e1dbe
johnobodai <j.obodai@alustudent.com>
2023-07-21 03:51:29 +0000

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,8 +8,31 @@ int _putchar(char i);
 /* checks for uppercase character */
 int _isupper(int c);
 
+/* checks for a digit */
+int _isdigit(int c);
 
+/* multiplies two integers */
+int mul(int a, int b);
 
+/* prints the numbers */
+void print_numbers(void);
+
+/* print the number with exception */
+void print_most_number(void);
+
+/* prints 10 times the numbers form 0 to 14 */
+void more_numbers(void);
+
+/* draws a straight line */
+void print_line(int n);
+
+/* draws a diagonal line */
+void print_diagonal (int n);
+
+/* prints a square */
+void print_square (int size);
+
+/* fizz-buzz test */
 
 
 #endif
