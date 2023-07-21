11ed60dec09e8598d87f3b306f6b9e638df77e38
johnobodai <j.obodai@alustudent.com>
2023-07-21 04:31:45 +0000

    Add function print_most_numbers to print numbers from 0 to 9 excluding 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers from 0 to 9
- *Just dont print2 and 4
+ * print_most_numbers - Prints numbers from 0 to 9 excluding 2 and 4, followed by a new line.
+ *
+ * Description: This function prints the numbers from 0 to 9 (excluding 2 and 4)
+ *              to the standard output followed by a new line.
+ *
  * Return: void
  */
-
 void print_most_numbers(void)
 {
-	int x;
+    int x;
 
-	for (x = 0; x <= 9; x++)
-	{
-		if (x != 2 && x != 4)
-		{
-			_putchar(x + '0');
-		}
-	}
-	_putchar('\n');
+    for (x = 0; x <= 9; x++)
+    {
+        if (x != 2 && x != 4)
+        {
+            _putchar(x + '0');
+        }
+    }
+    _putchar('\n');
 }
+
