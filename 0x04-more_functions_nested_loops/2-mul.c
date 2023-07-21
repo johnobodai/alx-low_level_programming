7917c6d657f85f28d8bf9b189b3de9d3bc35e43f
johnobodai <j.obodai@alustudent.com>
2023-07-21 03:58:27 +0000

    Initial commit:Add mul fucntion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply with the first number
+ *
+ * Return: The product of the two integers
+ */
+int mul(int a, int b)
+{
+	int result
+	result = a * b;
+	return (result);
+}
+
