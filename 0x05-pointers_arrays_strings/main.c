a98024e54761d06704809234c8a2d0223519b80c
johnobodai <j.obodai@alustudent.com>
2023-07-26 03:33:20 +0000

    compiled strlen

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,5 +1,18 @@
 #include "main.h"
-/**
- *  Add comments here.
- */
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
