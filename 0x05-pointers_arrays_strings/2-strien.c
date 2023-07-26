6fcf1aa7c35ef28be9d5716c8dd854f8d5c41855
johnobodai <j.obodai@alustudent.com>
2023-07-26 03:29:37 +0000

    Add _strlen function

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen - calculate the length of string
+ * @s: the string whose length is to be calculated
+ *
+ * description: the function is intended to calculate the length of a given
+ *              string and return the length as an integer
+ *
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return count;
+}
